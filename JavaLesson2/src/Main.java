456b5c6f79245aa1f5a6f7820ee654eb0e25445e
Vera Bylinskaia <iagyar@list.ru>
2024-10-21 19:42:24 +0300

    Задач

--- JavaLesson2/src/Main.java
+++ JavaLesson2/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 //        используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
 //        Если значение null, то параметр не должен попадать в запрос.
 //        Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+        System.out.println("Задача 1");
     }
 }
