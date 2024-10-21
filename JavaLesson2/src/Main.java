661a85bee094c62f7659f313e16dd67b69d853cc
Vera Bylinskaia <iagyar@list.ru>
2024-10-21 19:39:32 +0300

    Задача

---
+++ JavaLesson2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
+//        используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+//        Если значение null, то параметр не должен попадать в запрос.
+//        Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+    }
+}
